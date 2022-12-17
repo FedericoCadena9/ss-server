e7d27834400c6508148e64b617259d6e2f9e69cf
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2022-12-17 08:36:24 -0600

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'https://strapi-production-90d4.up.railway.app/'),
+  host: env('HOST', '127.0.0.1'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array("APP_KEYS"),
